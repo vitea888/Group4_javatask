ebe60ce658d6c9714b9de86a1d344dd57180dc4b
vitea888 <vitea88@gmail.com>
2025-10-22 13:39:32 -0500

    Created package and class

---
+++ src/instructor/Task1.java
@@ -0,0 +1,11 @@
+package instructor;
+/*
+      QUESTION 1 -Numbers  ---Odd & Even
+       Write a method which can identify if a given number odd or even
+
+       Ex:
+       identify(5) -->"Odd"
+       identify (6) --> "Even"
+      */
+public class Task1 {
+}
---
+++ src/victor2/Task1.java
@@ -0,0 +1,11 @@
+package victor2;
+/*
+      QUESTION 1 -Numbers  ---Odd & Even
+       Write a method which can identify if a given number odd or even
+
+       Ex:
+       identify(5) -->"Odd"
+       identify (6) --> "Even"
+      */
+public class Task1 {
+}
