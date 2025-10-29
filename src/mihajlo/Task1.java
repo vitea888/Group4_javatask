b051971d853b76a2b8264d59b17d7c0a5084e767
mihajlo3492 <mihajlompetkovic@gmail.com>
2025-10-29 10:46:22 -0400

    oddOrEven method added

--- src/mihajlo/Task1.java
+++ src/mihajlo/Task1.java
@@ -9,7 +9,17 @@ package mihajlo;
       */
 public class Task1 {
 
+    public static String oddOrEven(int num){
+        if (num % 2 == 0){
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println(oddOrEven(5));
+        System.out.println(oddOrEven(6));
     }
 }
