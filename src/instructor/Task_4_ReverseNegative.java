40e20f46e424dfb73a65ef69558b9a7a24a8327f
OwenEST <owen.constantine@cydeo.com>
2025-11-17 13:22:54 +0300

    Task 3 solution and Task 4 question

---
+++ src/instructor/Task_4_ReverseNegative.java
@@ -0,0 +1,10 @@
+package instructor;
+
+/* Numbers -- Reverse negative number
+Write a return method that can reverse
+negative number and return it as int */
+
+
+
+public class Task_4_ReverseNegative {
+}
