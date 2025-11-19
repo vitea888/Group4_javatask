93c8330a2675b1eb0183af741564e7a8c5a05b7d
OwenEST <owen.constantine@cydeo.com>
2025-11-19 17:23:52 +0300

    Task 4 solution and Task 5 question

--- src/instructor/Task_4_ReverseNegative.java
+++ src/instructor/Task_4_ReverseNegative.java
@@ -1,4 +1,4 @@
-package instructor;
+package Instructor;
 
 /* Numbers -- Reverse negative number
 Write a return method that can reverse
@@ -7,4 +7,48 @@ negative number and return it as int */
 
 
 public class Task_4_ReverseNegative {
+
+    public static int reverseNegativeNumber(int a){
+        return a*(-1);
+    }
+
+    public static int reverseNum(int  num) {
+
+        String str = new StringBuilder(""+num).reverse().toString();
+
+        if(num < 0) {
+            str = "-"+str.substring(0, str.length()-1);
+        }
+
+        return Integer.valueOf(str);
+    }
+
+    public static int revNum(int number){
+        if(number < 10 && number >= 0){
+            return number;
+        }
+
+        int result = 0;
+        while (number !=0) {
+            result = result * 10 + number % 10; // 1 , 14, 145
+            number /= 10; //54, 5, 0
+        }
+
+        return result;//145
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(reverseNegativeNumber(-10));
+        System.out.println("revNum() = " + revNum(-541));
+        System.out.println(reverseNum(-53));
+    }
+
+
+
+
+
+
+
+
 }
---
+++ src/instructor/Task_5_FrequencyOfCharacters.java
@@ -0,0 +1,8 @@
+package instructor;
+
+  /* Write a return method that can find the frequency of characters
+    Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2  */
+
+
+public class Task_5_FrequencyOfCharacters {
+}
